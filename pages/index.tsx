157faf5b74c0536ea8b0cc5161adeae236b3aa83
zelrdev <zelr.apps@gmail.com>
2023-02-01 15:20:06 +0000

    feat: nicer ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 
 export default function IndexPage() {
   const [game, setGame] = useState<Game | undefined>();
-  const [cooldown, setCooldown] = useState(true);
+  const [cooldown, setCooldown] = useState(false);
   const [copied, setCopied] = useState(false);
   const [checked, setChecked] = useState(false);
 
@@ -30,18 +30,18 @@ export default function IndexPage() {
         window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
       document.documentElement.classList.add("dark");
-      themeToggleLightIcon.classList.remove("hidden");
+      themeToggleLightIcon.classList.remove("opacity-0");
     } else {
       document.documentElement.classList.remove("dark");
-      themeToggleDarkIcon.classList.remove("hidden");
+      themeToggleDarkIcon.classList.remove("opacity-0");
     }
 
     const themeToggleBtn = document.getElementById("theme-toggle");
 
     themeToggleBtn.addEventListener("click", function () {
       // toggle icons inside button
-      themeToggleDarkIcon.classList.toggle("hidden");
-      themeToggleLightIcon.classList.toggle("hidden");
+      themeToggleDarkIcon.classList.toggle("opacity-0");
+      themeToggleLightIcon.classList.toggle("opacity-0");
 
       // if set via local storage previously
       if (localStorage.getItem("color-theme")) {
@@ -66,20 +66,20 @@ export default function IndexPage() {
     });
   }, []);
 
-  const getRandomGame = async () => {
-    setCooldown(true);
+  const getRandomGame = async (checked: boolean) => {
     setGame(undefined);
     const req = await fetch(
       `/api/get-random?popular=${checked ? "yes" : "no"}`
     );
     if (!req.ok) return;
     const reqJson = await req.json();
+    setCooldown(true);
     setGame(reqJson);
     setTimeout(() => setCooldown(false), 500);
   };
 
   useEffect(() => {
-    getRandomGame();
+    getRandomGame(checked);
   }, []);
 
   return (
@@ -88,11 +88,11 @@ export default function IndexPage() {
         <button
           id="theme-toggle"
           type="button"
-          className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+          className="text-slate-500 dark:text-slate-400 hover:bg-slate-100 active:opacity-50 dark:hover:bg-slate-700 focus:outline-none transition focus:ring-slate-200 dark:focus:ring-slate-700 rounded-lg text-sm p-2.5"
         >
           <svg
             id="theme-toggle-dark-icon"
-            className="w-5 h-5 hidden"
+            className="w-5 h-5 transition-opacity absolute ease-in-out opacity-0"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -101,7 +101,7 @@ export default function IndexPage() {
           </svg>
           <svg
             id="theme-toggle-light-icon"
-            className="w-5 h-5 hidden"
+            className="w-5 h-5 transition-opacity ease-in-out opacity-0"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -117,47 +117,66 @@ export default function IndexPage() {
       <div className="h-screen w-screen flex justify-center items-center dark:bg-slate-900 transition">
         <div className="space-y-7">
           <div>
-            <h1 className="text-3xl md:text-5xl text-center text-blue-900 dark:text-white font-bold transition">
+            <h1 className="text-3xl pointer-events-none md:text-5xl text-center text-red-600 dark:text-white font-bold transition">
               Random Roblox Game
             </h1>
-            <p className="text-center dark:text-white text-xl pt-2 transition">
-              made with ❤️ by{" "}
-              <a
-                href="https://portfolio-zelr.vercel.app/"
-                className="text-red-600 hover:text-red-900 transition font-semibold dark:text-red-400 dark:hover:text-red-500"
-                target="_blank"
-              >
-                zelr
-              </a>
-            </p>
+            <div className="flex gap-2 flex-row-reverse items-center justify-center">
+              <p className="text-center dark:text-white text-xl pt-2 transition">
+                made with ❤️ by{" "}
+                <a
+                  href="https://portfolio-zelr.vercel.app/"
+                  className="text-red-600 hover:text-red-900 transition font-semibold dark:text-red-400 dark:hover:text-red-500"
+                  target="_blank"
+                >
+                  zelr
+                </a>
+              </p>
+              <p className="text-center pointer-events-none dark:text-white text-xl pt-2 transition">
+                -
+              </p>
+              <p className="text-center dark:text-white text-xl pt-2 transition">
+                <a
+                  href="https://github.com/zelrdev/roblox-random-gen"
+                  className="text-red-600 hover:text-red-900 transition font-semibold dark:text-red-400 dark:hover:text-red-500"
+                  target="_blank"
+                >
+                  open source
+                </a>
+              </p>
+            </div>
           </div>
           <div className="flex justify-center items-center w-full">
-            <div className="bg-slate-50 dark:text-white dark:bg-slate-800 rounded-md p-5 flex flex-col md:flex-row gap-4 transition">
+            <div className="bg-slate-50 border dark:border-slate-700 dark:text-white dark:bg-slate-800  rounded-md p-5 flex flex-col md:flex-row gap-4 transition">
               {game ? (
                 <img
-                  className="rounded-md w-[150px] h-[150px] shadow"
+                  draggable="false"
+                  className="rounded-lg w-[150px] h-[150px] shadow"
                   src={game?.image}
                 />
               ) : (
-                <div className="animate-pulse w-[150px] h-[150px] dark:bg-gray-700 bg-gray-300 rounded"></div>
+                <div className="animate-pulse w-[150px] h-[150px] dark:bg-slate-700 bg-slate-300 rounded-lg"></div>
               )}
               <div className="space-y-2">
-                <div>
+                <div className="pointer-events-none">
                   {game ? (
-                    <h2 className="md:text-xl font-bold">{game?.name}</h2>
+                    <h2 className={`font-bold md:text-xl`}>{game?.name}</h2>
                   ) : (
-                    <div className="animate-pulse w-1/2 h-7 dark:bg-gray-700 bg-gray-300 rounded"></div>
+                    <div className="animate-pulse w-1/2 h-7 dark:bg-slate-700 bg-slate-300 rounded"></div>
                   )}
                 </div>
-                <div>
+                <div className="pointer-events-none">
                   <p className="text-ellipsis md:w-[500px] h-24 md:h-16 overflow-y-auto whitespace-pre-wrap text-sm">
                     {game ? (
-                      game?.desc
+                      game?.desc ? (
+                        game?.desc
+                      ) : (
+                        "No description"
+                      )
                     ) : (
                       <div className="space-y-1">
-                        <div className="animate-pulse w-1/6 h-4 dark:bg-gray-700 bg-gray-300 rounded"></div>
-                        <div className="animate-pulse w-full h-4 dark:bg-gray-700 bg-gray-300 rounded"></div>
-                        <div className="animate-pulse w-5/6 h-4 dark:bg-gray-700 bg-gray-300 rounded"></div>
+                        <div className="animate-pulse w-1/6 h-4 dark:bg-slate-700 bg-slate-300 rounded"></div>
+                        <div className="animate-pulse w-full h-4 dark:bg-slate-700 bg-slate-300 rounded"></div>
+                        <div className="animate-pulse w-5/6 h-4 dark:bg-slate-700 bg-slate-300 rounded"></div>
                       </div>
                     )}
                   </p>
@@ -166,21 +185,23 @@ export default function IndexPage() {
                   {game ? (
                     <>
                       <a
-                        target="_blank"
-                        className="bg-green-700 flex gap-2 items-center hover:bg-green-500 font-semibold duration-300 transition p-2 px-6 rounded-lg text-white"
+                        draggable="false"
+                        className="dark:bg-green-700 active:opacity-50 bg-green-500 hover:bg-green-400 flex gap-2 items-center dark:hover:bg-green-500 font-semibold duration-300 transition p-2 px-6 rounded-xl text-white"
                         href={`roblox://placeId=${game.placeId}`}
                       >
                         <PlayIcon height={20} /> Play
                       </a>
                       <a
+                        draggable="false"
                         target="_blank"
-                        className="bg-gray-700 flex gap-2 items-center hover:bg-gray-500 font-semibold duration-300 transition p-2 px-4 rounded-lg text-white"
+                        className="dark:bg-slate-700 active:opacity-50 flex gap-2 items-center dark:hover:bg-slate-500 bg-slate-200 hover:bg-slate-300 font-semibold duration-300 transition p-2 px-4 rounded-xl dark:text-white"
                         href={`https://www.roblox.com/games/${game.placeId}`}
                       >
                         <ArrowUpRightIcon height={20} /> Open Link
                       </a>
                       <a
-                        className="bg-gray-700 cursor-pointer flex gap-2 items-center hover:bg-gray-500 font-semibold duration-300 transition p-2 px-4 rounded-lg text-white"
+                        draggable="false"
+                        className="dark:bg-slate-700 active:opacity-50 bg-slate-200 hover:bg-slate-300 cursor-pointer flex gap-2 items-center dark:hover:bg-slate-500 font-semibold duration-300 transition p-2 px-4 rounded-xl dark:text-white"
                         onClick={() => {
                           setCopied(true);
                           navigator.clipboard.writeText(
@@ -205,9 +226,9 @@ export default function IndexPage() {
                     </>
                   ) : (
                     <>
-                      <div className="animate-pulse w-28 h-10 dark:bg-gray-700 bg-gray-300 rounded-lg"></div>
-                      <div className="animate-pulse w-32 h-10 dark:bg-gray-700 bg-gray-300 rounded-lg"></div>
-                      <div className="animate-pulse w-14 h-10 dark:bg-gray-700 bg-gray-300 rounded-lg"></div>
+                      <div className="animate-pulse w-28 h-10 dark:bg-slate-700 bg-slate-300 rounded-xl"></div>
+                      <div className="animate-pulse w-[8.5rem] h-10 dark:bg-slate-700 bg-slate-300 rounded-xl"></div>
+                      <div className="animate-pulse w-12 h-10 dark:bg-slate-700 bg-slate-300 rounded-xl"></div>
                     </>
                   )}
                 </div>
@@ -216,26 +237,58 @@ export default function IndexPage() {
           </div>
           <div className="flex flex-col gap-2 justify-center items-center">
             <div className="flex justify-center">
-              <div>
+              <div className="relative inline-block w-10 mr-2 align-middle select-none">
                 <input
-                  onChange={(event) => setChecked(event.target.checked)}
-                  className="form-check-input h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                   type="checkbox"
+                  name="toggle"
+                  onChange={(event) => {
+                    setChecked(event.target.checked);
+                    getRandomGame(event.target.checked);
+                  }}
+                  id="toggle"
                   checked={checked}
+                  className="checked:bg-red-500 transition-all ease-in-out dark:checked:bg-red-500 outline-none focus:outline-none right-4 checked:right-0 duration-200 absolute block w-6 h-6 rounded-full dark:bg-slate-900 bg-white border-4 dark:border-slate-700 appearance-none cursor-pointer"
                 />
                 <label
-                  className="form-check-label inline-block text-gray-800 dark:text-gray-100"
-                  htmlFor="flexCheckDefault"
-                >
-                  Frontpage games only
-                </label>
+                  htmlFor="toggle"
+                  className="block h-6 overflow-hidden bg-slate-300 dark:bg-slate-800 rounded-full cursor-pointer"
+                ></label>
               </div>
+              <span className="font-medium text-slate-400 pointer-events-none">
+                Frontpage games only
+              </span>
             </div>
             <button
               disabled={cooldown}
-              className="bg-red-900 hover:bg-red-700 flex gap-2 items-center disabled:opacity-50 active:scale-95 font-semibold duration-300 transition p-3 px-6 rounded-lg text-white"
-              onClick={() => getRandomGame()}
+              className={`dark:bg-red-900 relative group disabled:pointer-events-none disabled:opacity-50 mt-5 dark:hover:bg-red-700 bg-red-500 hover:bg-red-400 flex gap-2 items-center active:opacity-50 font-semibold duration-300 transition p-3 px-6 rounded-2xl text-white ${
+                !game && "pointer-events-none"
+              }`}
+              onClick={() => getRandomGame(checked)}
             >
+              <div
+                role="status"
+                className={`dark:bg-red-900 bg-red-500 group-disabled:dark:bg-opacity-50 transition-opacity ease-in-out duration-300 w-[117px] absolute top-3 ${
+                  game ? "opacity-0" : "opacity-100"
+                }`}
+              >
+                <svg
+                  aria-hidden="true"
+                  className=" h-6 w-[117px] animate-spin text-transparent fill-white"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
               <PlusIcon height={24} /> New Game
             </button>
           </div>
