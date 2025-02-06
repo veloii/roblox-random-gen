d33c44282c79dbd327447c430b3f60617290f277
TheEmptynessProject <89524326+TheEmptynessProject@users.noreply.github.com>
2025-02-06 23:32:46 +0000

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,23 @@
 export interface Game {
   name: string;
   creatorName: string;
-  price: number | null;
+  price: number;
   desc: string;
-  image: string;
-  imageToken: string;
   universeId: number;
   placeId: number;
-  totalUpVotes: number;
-  totalDownVotes: number;
+  image: string;
+  stats: GameStats;
+  totalUpVotes?: number;
+  totalDownVotes?: number;
 }
+
+export interface GameStats {
+  playing: number;
+  visits: number;
+  favorites: number;
+  maxPlayers: number;
+  created: string;
+  updated: string;
+  gamePasses: number;
+}
+
