550dcb393edb9fff45c2977d307eb17e2283b8b8
TheEmptynessProject <89524326+TheEmptynessProject@users.noreply.github.com>
2025-02-07 01:30:20 +0000

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,44 @@
 export interface Game {
   name: string;
-  creatorName: string;
+  creator: Creator;
   price: number | null;
   desc: string;
-  image: string;
-  imageToken: string;
   universeId: number;
   placeId: number;
-  totalUpVotes: number;
-  totalDownVotes: number;
+  image: string;
+  stats: GameStats;
+  sourceName: string;
+  sourceDescription: string;
+  allowedGearGenres: string[];
+  allowedGearCategories: string[];
+  isGenreEnforced: boolean;
+  copyingAllowed: boolean;
+  created: string;
+  updated: string;
+  studioAccessToApisAllowed: boolean;
+  createVipServersAllowed: boolean;
+  universeAvatarType: string;
+  genre: string;
+  genre_l1: string;
+  genre_l2: string;
+  isAllGenre: boolean;
+  isFavoritedByUser: boolean;
+  totalUpVotes?: number;
+  totalDownVotes?: number;
+}
+
+export interface Creator {
+  id: number;
+  name: string;
+  type: string;
+  isRNVAccount: boolean;
+  hasVerifiedBadge: boolean;
+}
+
+export interface GameStats {
+  playing: number;
+  visits: number;
+  favoritedCount: number;
+  maxPlayers: number;
+  gamePasses?: number;
 }
